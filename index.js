25aea1cb9edabb7c66b2136f3805281a0ded8b85
Ben Kagan <kagan.benjamin@gmail.com>
2020-01-29 12:51:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+let finalArray = [];
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        finalArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return finalArray;
+}
+
+function countDown(int) {
+    for (let i = 0; i < (int + 1); i++) {
+        console.log(int - i);
+    }
+}
